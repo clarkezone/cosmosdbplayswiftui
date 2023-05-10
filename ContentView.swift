25cc2c89f9edfd5e46af5d1df9ee0e97873a9788
James Clarke <james@clarkezone.net>
2023-05-10 09:37:31 +0200

    Refactor

--- ContentView.swift
+++ ContentView.swift
@@ -7,30 +7,31 @@ struct ContentView: View {
     @AppStorage("accountname") private var accountname = ""
     @AppStorage("databaseid") private var databaseid = ""
     @AppStorage("collectionid") private var collectionid = ""
+    @AppStorage("sql") private var sqltext = ""
+    
+    func callCosmos() {
+        let verb = $sqltext.wrappedValue != "" ? "POST" : "GET"
+        Task.init {
+            do {
+                let json = try await callCosmosDB(verb:verb, accountName: $accountname.wrappedValue, masterKey: $cosmoskey.wrappedValue, databaseId: $databaseid.wrappedValue, collectionId: $collectionid.wrappedValue, 
+                                                  sqlQuery: $sqltext.wrappedValue)
+                print("JSON Response: \(json)")
+                
+            } catch {
+                print("Error: \(error)")
+            }
+        }
+    }
     
     var body: some View {
         VStack {
-            Text("Cosmos Key:")
             TextField("Enter key", text: $cosmoskey).padding()
-            Text("Accountname:")
             TextField("Enter account name", text: $accountname).padding()
-            Text("Database ID:")
             TextField("database id", text: $databaseid).padding()
-            Text("Collection id:")
             TextField("collectionid", text: $collectionid).padding()
+            TextField("sql", text: $sqltext).padding()
             Button("CallCosmos") {
-                let verb = "POST"
-                let sqlQuery = "SELECT top 10 * FROM geocache p order by p.Timestamp desc"
-                Task.init {
-                    do {
-                        let json = try await callCosmosDB(verb:verb, accountName: $accountname.wrappedValue, masterKey: $cosmoskey.wrappedValue, databaseId: $databaseid.wrappedValue, collectionId: $collectionid.wrappedValue, sqlQuery: sqlQuery)
-                        print("JSON Response: \(json)")
-                        
-                    } catch {
-                        print("Error: \(error)")
-                    }
-                }
-
+                callCosmos()
             }
         }
     }
--- CosmosDB.swift
+++ CosmosDB.swift
@@ -3,14 +3,19 @@ import CryptoKit
 import Foundation
 
 func callCosmosDB(verb:String, accountName: String, masterKey: String, databaseId: String, collectionId: String, sqlQuery: String) async throws -> Any {
-    let resourceType = "docs"
-    let resourceId = "dbs/\(databaseId)/colls/\(collectionId)"
-    //    let resourceType = "dbs"
-    //let resourceId = ""
-    //    let resourceLink = "dbs"
-    let resourceLink = "dbs/\(databaseId)/colls/\(collectionId)/docs"
+    var resourceId = ""
+    var resourceLink = ""
+    var resourceType = ""
     
-    let urlString = "https://\(accountName).documents.azure.com/dbs/\(databaseId)/colls/\(collectionId)/docs"
+    if sqlQuery != "" {
+        resourceType = "dbs"
+        resourceLink = "dbs"        
+    } else {
+        resourceType = "docs"
+        resourceId = "dbs/\(databaseId)/colls/\(collectionId)"
+        resourceLink = "dbs/\(databaseId)/colls/\(collectionId)/docs"      
+    }
+    let urlString = "https://\(accountName).documents.azure.com/\(resourceLink)"
     //  let urlString = "https://\(accountName).documents.azure.com/dbs"
     guard let url = URL(string: urlString) else { throw NSError(domain: "Invalid URL", code: 1, userInfo: nil) }
     
@@ -18,29 +23,25 @@ func callCosmosDB(verb:String, accountName: String, masterKey: String, databaseI
     request.httpMethod = verb
     
     let date = generateUTCDate()
-    //let date = DateFormatter.azureDateFormatter.string(from: Date())
     request.addValue(date, forHTTPHeaderField: "x-ms-date")
     request.addValue("application/json", forHTTPHeaderField: "Accept")
     request.addValue("application/query+json", forHTTPHeaderField: "Content-Type")
-    request.addValue("true", forHTTPHeaderField: "x-ms-documentdb-isquery")
-    //    request.addValue("2018-12-31", forHTTPHeaderField: "x-ms-version") // Add the x-ms-version header with the appropriate version value
     request.addValue("2015-08-06", forHTTPHeaderField: "x-ms-version") // Add the x-ms-version header with the appropriate version value
-    request.addValue("true", forHTTPHeaderField: "x-ms-documentdb-query-enablecrosspartition")
-    
-    
-    //    if let authToken = generateAuthToken(verb: verb, resourceId: resourceId, resourceType: resourceType, date: date, masterKey: masterKey) {
-    //request.addValue(authToken, forHTTPHeaderField: "Authorization")
-    //}
-    
+    if sqlQuery != "" {
+        request.addValue("true", forHTTPHeaderField: "x-ms-documentdb-isquery")
+        request.addValue("true", forHTTPHeaderField: "x-ms-documentdb-query-enablecrosspartition")
+    }
     if let authToken = generateMasterKeyAuthorizationSignature(verb:verb, resourceId: resourceId, resourceType: resourceType, key: masterKey, keyType: "master", tokenVersion: "1.0") {
         request.addValue(authToken, forHTTPHeaderField: "Authorization")
     }
     
-    let queryPayload = [
-        "query": sqlQuery
-    ]
-    
-    request.httpBody = try JSONSerialization.data(withJSONObject: queryPayload, options: [])
+    if sqlQuery != "" {
+        let queryPayload = [
+            "query": sqlQuery
+        ]
+        
+        request.httpBody = try JSONSerialization.data(withJSONObject: queryPayload, options: [])
+    }
     
     let (data, _) = try await URLSession.shared.data(for: request)
     let json = try JSONSerialization.jsonObject(with: data, options: [])
